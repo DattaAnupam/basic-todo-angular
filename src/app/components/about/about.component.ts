95c158775378ff5fe7d8348fec1111fa410368e0
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-07-12 17:00:54 +0530

    add git repo url into about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  gitRepoUrl: string = 'https://github.com/DattaAnupam/basic-todo-angular';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
